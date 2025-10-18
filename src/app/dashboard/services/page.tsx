e44242d22a37ad436d19a5083feb48c83138bfed
MU30 <salimusonda1@gmail.com>
2025-10-18 15:31:53 +0000

    Services is showing: 404 This page could not be found., fix it

---
+++ src/app/dashboard/services/page.tsx
@@ -0,0 +1,9 @@
+import { ServicesTable } from "@/components/dashboard/provider/services-table";
+
+export default function ServicesPage() {
+  return (
+    <div className="space-y-8">
+      <ServicesTable />
+    </div>
+  );
+}
